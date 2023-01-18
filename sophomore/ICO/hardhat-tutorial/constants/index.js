f5265a562c599bf7d944bcf6a192c4af49163bc1
Anil Maharjan <simpleanilcha@gmail.com>
2023-01-18 17:21:28 +0545

    add: hardhat-tutorial for ICO project

---
+++ sophomore/ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x521737B8c5C113aF7D25ce070a4BDAcC47EBEc55";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
