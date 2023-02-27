38b2f28f16c9ca7a6b98c942fa9ca1a445a8455e
Anil Maharjan <simpleanilcha@gmail.com>
2023-02-27 16:26:38 +0545

    update: addresses

--- sophomore/DAO-Tutorial/hardhat-tutorial/hardhat.config.js
+++ sophomore/DAO-Tutorial/hardhat-tutorial/hardhat.config.js
@@ -5,11 +5,12 @@ const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
+  defaultNetwork: "goerli",
   networks: {
     goerli: {
-      url: QUICKNODE_HTTP_URL,
-      accounts: [PRIVATE_KEY],
+      url: process.env.QUICKNODE_HTTP_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
